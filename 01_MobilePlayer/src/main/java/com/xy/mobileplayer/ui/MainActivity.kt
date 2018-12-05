5554928155043850d394367fb38bdfbb720e85a6
xuyang <xuyang@okay.cn>
2018-12-05 10:54:16 +0800

    git init

---
+++ 01_MobilePlayer/src/main/java/com/xy/mobileplayer/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.xy.mobileplayer.ui
+
+import com.xy.mobileplayer.R
+import com.xy.mobileplayer.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun getLayoutid(): Int {
+       return R.layout.activity_main
+    }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+}
