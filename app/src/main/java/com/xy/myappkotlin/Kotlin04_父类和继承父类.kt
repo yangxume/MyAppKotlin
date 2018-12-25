bad1a54e31c934ffdd8602bb4131c2937835cc6a
xuyang <xuyangme@126.com>
2018-12-25 21:21:59 +0800

    父类和继承父类

--- app/src/main/java/com/xy/myappkotlin/Kotlin04_父类和继承父类.kt
+++ app/src/main/java/com/xy/myappkotlin/Kotlin04_父类和继承父类.kt
@@ -2,19 +2,41 @@ package com.xy.myappkotlin
 
 fun main(args : Array<String>){
 
+    var instance: BaseTest<String>
+    instance = Test<String>("本类是")
+    //父类对象调用子类对象的方法
+    instance.test()
 
+    instance.closeTest()
 
 }
 
-class BaseTest<T> constructor(val t:T){
-
-
-    fun test(){
-        println("${t} BaseTest 父类")
+/**
+ * open关键字：
+ * 1. 修饰父类:open class BaseTest
+ * 2. 主构造方法中声明的属性:open val t: T
+ * 3. 修饰方法:open fun test(){ }
+ *
+ * 默认情况下，Kotlin中的类，属性，方法都是被final修饰的，不允许继承或者覆盖重写。
+ */
+open class BaseTest<T>( open val t: T) {
+    open fun test() {
+        println("${t} BaseTest父类 ")
     }
-
     fun closeTest(){
-        println("父类中到方法不能被")
+        println("父类中不能被子类重新复写的方法")
     }
-
+}
+/**
+ *  继承自定义的BaseTest父类
+ *  覆盖属性t
+ *  复写test（）
+ */
+class Test<T> constructor(override var t: T) : BaseTest<T>(t) {
+    /**
+     * 复写父类中的方法
+     */
+    override fun test() {
+        println("${t} Test子类 ")
+    }
 }
